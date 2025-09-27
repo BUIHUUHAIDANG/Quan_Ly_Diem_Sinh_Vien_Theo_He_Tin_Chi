954c964314668811b2735daa1f466bd89f00dc3a
VUANHKHOA <khoava06@gmail.com>
2025-09-28 00:50:55 +0700

    Add files via upload
    Cài đặt hàm strcmp
    Thêm chức năng nhập môn học
    (Chưa lưu vào file)

---
+++ Khoa.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+const int MAX_LOPSV = 10000;
+const int MAX_LTC=10000;
+struct MonHoc
+{
+    char MAMH[11];
+    char TENMH[51];
+    int STCLT;
+    int STCTH;
+    int height;
+};
+struct nodeMH //tree
+{
+    MonHoc mh;
+    nodeMH *left;
+    nodeMH *right;
+};
+typedef nodeMH* treeMH;
+struct SinhVien //tree
+{
+    char MASV[16];
+    char HO[51];
+    char TEN[11];
+    char PHAI[4];
+    char SODT[16];
+    char Email[50];
+};
+struct nodeSV
+{
+    SinhVien sv;
+    nodeSV *next;
+};
+typedef nodeSV* PTRSV;
+struct LopSV
+{
+    char MALOP[16];
+    char TENLOP[51];
+    PTRSV FristSV=nullptr;
+};
+struct DS_LOPSV
+{
+    int n=0;
+    LopSV* nodes[MAX_LOPSV];
+};
+struct DangKy
+{
+    char MASV[16];
+    float DIEM;
+};
+struct nodeDK
+{
+    DangKy dk;
+    nodeDK *next;
+};
+typedef nodeDK* PTRDK;
+struct LopTinChi
+{
+    int MALOPTC;
+    char MAMH[11];
+    char NienKhoa[10];
+    int Hocky,Nhom,sosvmin,sosvmax;
+    bool huylop =false;
+    PTRDK dssvdk=nullptr;
+};
+
+struct nodeLTC 
+{
+    LopTinChi ltc;
+    LopTinChi *nodes[MAX_LTC];
+};
+
+int strcmp(char *s, char *t) {
+    while (*s && *t && (*s == *t)) {
+        s++;
+        t++;
+    }
+    return (*s - *t);
+}
+
+treeMH* NhapMonHoc(treeMH &t) {
+    MonHoc mh;
+    cout << "Nhap ma mon hoc: ";
+    cin >> mh.MAMH;
+    if (strcmp(mh.MAMH, "0") == 0) return nullptr;
+    cout << "Nhap ten mon hoc: ";
+    cin.ignore();
+    cin.getline(mh.TENMH, 51);
+    cout << "Nhap so tin chi ly thuyet: ";
+    cin >> mh.STCLT;
+    cout << "Nhap so tin chi thuc hanh: ";
+    cin >> mh.STCTH;
+    mh.height = 1;
+
+    if (t == nullptr) {
+        t = new nodeMH;
+        t->mh = mh;
+        t->left = nullptr;
+        t->right = nullptr;
+    }
+    
+    else {
+        if (strcmp(mh.MAMH, t->mh.MAMH) < 0) {
+            NhapMonHoc(t->left);
+        }
+        else if (strcmp(mh.MAMH, t->mh.MAMH) > 0) {
+            NhapMonHoc(t->right);
+        }
+        else {
+            cout << "Ma mon hoc da ton tai. Vui long nhap lai." << endl;
+        }
+    }
+    return &t;
+}
