f970be93bf05bbacae25bf3011a9bb5b235dd54b
VUANHKHOA <khoava06@gmail.com>
2025-11-18 12:51:30 +0700

    update

--- MonHoc.h
+++ MonHoc.h
@@ -5,6 +5,24 @@
 using namespace std;
 
 // -------------------- CẤU TRÚC --------------------
+struct SinhVien //node
+{
+    char MASV[16];
+    char HO[51];
+    char TEN[11];
+    char PHAI[4];
+    char SODT[16];
+    char Email[50];
+};
+
+struct nodeSV
+{
+    SinhVien sv;
+    nodeSV *next;
+};
+
+typedef nodeSV* PTRSV;
+
 struct MonHoc
 {
     char MAMH[11];
@@ -35,17 +53,23 @@ treeMH RotateRight(treeMH y);
 treeMH RotateLeft(treeMH x);
 treeMH CheckandRotation(treeMH t);
 treeMH Insert(treeMH t, MonHoc mh);
-treeMH XoaMH(treeMH &t, char MAMH[]);
 
 // -------------------- HÀM FILE --------------------
 void LuuMonHoc(treeMH t, string filename);
 treeMH DocMonHoc(string filename);
 
 // -------------------- HÀM HỖ TRỢ --------------------
+PTRLTC FilterLTC(PTRLTC First, char nienkhoa[], int hocky);
+PTRSV findSinhVien(PTRSV First, char MASV[]);
 bool checkMH(treeMH t, MonHoc mh);
 void NhapMonHoc(treeMH &t);
+treeMH XoaMH(treeMH &t, char MAMH[]);
+void UndoSuaMH (treeMH &t, MonHoc mh);
 void SuaMH(treeMH &t, MonHoc mh);
+void UndoMH (treeMH &t);
 void InDSMH(treeMH t);
 bool timMonHoc(treeMH t, char mamh[]);
+void InLTC (PTRLTC &loptinchi, char nienkhoa[], int hocky, treeMH &t);
+void DangKyLTC (PTRLTC loptinchi, LopTinChi lop, PTRSV &dssv, treeMH &t);
 
 #endif
